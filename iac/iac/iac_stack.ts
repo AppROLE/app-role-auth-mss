b71b6a3455db34b83001edf64f5e23549bb9b04c
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-27 01:43:36 -0300

    feat: Add .env.example file with test stage configuration

---
+++ iac/iac/iac_stack.ts
@@ -0,0 +1,10 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class IacStack extends Stack {
+  constructor(scope: Construct, constructId: string, props?: StackProps) {
+    super(scope, constructId, props);
+
+    // The code that defines your stack goes here
+  }
+}
