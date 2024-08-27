3c39ca4b1394516b33912c4d99d0df7a191ad583
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-27 00:34:07 -0300

    feat: Add MongoDB connection and User model

---
+++ src/shared/infra/database/models/index.ts
@@ -0,0 +1,19 @@
+import { envs } from "../../../../envs";
+import mongoose from "mongoose";
+import User from "./user.model";
+
+
+const connectDB = async () => {
+  try {
+    if (!envs.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
+    await mongoose.connect(envs.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    process.exit(1);
+  }
+};
+
+export { connectDB, User };
