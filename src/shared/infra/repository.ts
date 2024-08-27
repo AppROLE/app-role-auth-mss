4aa54f82215a5e34f5f4db9bba5d805972d6244e
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-27 18:29:26 -0300

    feat: Add Repository class for managing user repositories

---
+++ src/shared/infra/repository.ts
@@ -0,0 +1,18 @@
+import { UserRepoMock } from "./mocks/user_repository_mock";
+import { UserRepositoryMongo } from "./database/repositories/user_repository_mongo";
+
+class RepositoryProps {
+  user_repo?: boolean = false;
+}
+
+export class Repository {
+  public UserRepo: UserRepositoryMongo | UserRepoMock;
+
+  constructor(props: RepositoryProps) {
+    if (props.user_repo) {
+      this.UserRepo = new UserRepositoryMongo();
+    } else {
+      this.UserRepo = new UserRepoMock();
+    }
+  }
+}
