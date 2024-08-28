cd57f7153069fd1f2a028b207e49300f677de53e
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-28 11:54:15 -0300

    refactor: Update user repository to use CognitoIdentityProviderClient for user management

---
+++ src/shared/infra/repositories/user/user_repository_cognito.ts
@@ -0,0 +1,35 @@
+import { generateConfirmationCode } from "@/shared/utils/generate_confirmation_code";
+import { User } from "../../../domain/entities/user";
+import { IUserRepository } from "../../../domain/irepositories/user_repository_interface";
+import { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider"
+
+
+export class UserRepositoryCognito implements IUserRepository {
+  userPoolId: string;
+  clientId: string;
+  client: CognitoIdentityProviderClient;
+
+  constructor(userPoolId: string, clientId: string) {
+    this.userPoolId = userPoolId;
+    this.clientId = clientId;
+    this.client = new CognitoIdentityProviderClient(
+      {
+        region: 'sa-east-1'
+      }
+    );
+  }
+
+  async forgotPassword(email: string): Promise<string> {
+    try {
+      const code = generateConfirmationCode()
+
+      // set code in user attributes
+    } catch (error) {}
+  }
+  getUserByEmail(email: string): Promise<User> {
+    throw new Error("Method not implemented.");
+  }
+  signUp(name: string, email: string, password: string, acceptedTerms: boolean): Promise<User> {
+    throw new Error("Method not implemented.");
+  }
+}
