72b32358a15efc4350c39cdffedb22dd0daecf4e
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-28 10:45:35 -0300

    refactor: Update IUserRepository interface to return null for getUserByEmail

--- src/shared/domain/irepositories/user_repository_interface.ts
+++ src/shared/domain/irepositories/user_repository_interface.ts
@@ -2,6 +2,11 @@ import { User } from "../entities/user";
 
 export interface IUserRepository {
   forgotPassword(email: string): Promise<string>;
-  getUserByEmail(email: string): Promise<User>;
-  signUp(name: string, email: string, password: string, acceptedTerms: boolean): Promise<User>;
+  getUserByEmail(email: string): Promise<User | null>;
+  signUp(
+    name: string,
+    email: string,
+    password: string,
+    acceptedTerms: boolean
+  ): Promise<User>;
 }
