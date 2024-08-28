2553dbf4898283604487245878f2a7ae3822ffc7
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-28 20:20:24 -0300

    refactor: Update user repository to use CognitoIdentityProviderClient for user management

--- src/shared/infra/repositories/user/user_repository_cognito.ts
+++ src/shared/infra/repositories/user/user_repository_cognito.ts
@@ -1,7 +1,8 @@
-import { generateConfirmationCode } from "@/shared/utils/generate_confirmation_code";
+import { generateConfirmationCode } from "../../../utils/generate_confirmation_code";
 import { User } from "../../../domain/entities/user";
 import { IUserRepository } from "../../../domain/irepositories/user_repository_interface";
-import { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider"
+import { AdminUpdateUserAttributesCommand, AdminUpdateUserAttributesCommandInput, CognitoIdentityProviderClient, ListUsersCommand, ListUsersCommandInput } from "@aws-sdk/client-cognito-identity-provider"
+import { NoItemsFound } from "../../../helpers/errors/usecase_errors";
 
 
 export class UserRepositoryCognito implements IUserRepository {
@@ -23,12 +24,65 @@ export class UserRepositoryCognito implements IUserRepository {
     try {
       const code = generateConfirmationCode()
 
+      await this.updateUserConfirmationCode(email, code);
+
+      return "";
+
+
       // set code in user attributes
-    } catch (error) {}
+    } catch (error: any) {
+      throw new Error("UserRepositoryCognito, Error on forgotPassword: " + error.message);
+    }
 
   }
-  getUserByEmail(email: string): Promise<User> {
-    throw new Error("Method not implemented.");
+
+  async updateUserConfirmationCode(email: string, confirmationCode: string): Promise<void> {
+    const user = await this.getUserByEmail(email);
+
+    if (!user) {
+      throw new Error("Usuário não encontrado.");
+    }
+
+    const params: AdminUpdateUserAttributesCommandInput = {
+      UserPoolId: this.userPoolId,
+      Username: user.userUsername as string, // Utilizando o Username do usuário encontrado
+      UserAttributes: [
+        {
+          Name: 'custom:confirmationCode',
+          Value: confirmationCode,
+        },
+      ],
+    };
+
+    const command = new AdminUpdateUserAttributesCommand(params);
+    await this.client.send(command);
+  
+  }
+  async getUserByEmail(email: string): Promise<User> {
+    try {
+      const params: ListUsersCommandInput = {
+        UserPoolId: this.userPoolId,
+        Filter: `email = "${email}"`,
+      };
+      const command = new ListUsersCommand(params);
+      const result = await this.client.send(command);
+      if (result.Users && result.Users.length > 0) {
+        const user = result.Users[0];
+        return new User({
+          name: user.Attributes?.find((attr) => attr.Name === 'name')?.Value as string,
+          email: user.Attributes?.find((attr) => attr.Name === 'email')?.Value as string,
+          username: user.Username as string,
+          nickname: user.Attributes?.find((attr) => attr.Name === 'custom:nickname')?.Value as string,
+          user_id: user.Attributes?.find((attr) => attr.Name === 'sub')?.Value as string,
+          password: user.Attributes?.find((attr) => attr.Name === 'password')?.Value as string,
+          
+        });
+      } else {
+        throw new NoItemsFound("email")
+      }
+    } catch (error : any) {
+      throw new Error("UserRepositoryCognito, Error on getUserByEmail: " + error.message);
+    }
   }
   signUp(name: string, email: string, password: string, acceptedTerms: boolean): Promise<User> {
     throw new Error("Method not implemented.");
