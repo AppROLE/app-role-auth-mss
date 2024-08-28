c6185b92fe0b5f623b0c7beddd36f3ae809901a8
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-28 10:45:48 -0300

    refactor: Improve forgot password use case and add email notification

--- tests/usecases/forgot_password_usecase.spec.ts
+++ tests/usecases/forgot_password_usecase.spec.ts
@@ -1,17 +1,21 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import { ForgotPasswordUseCase } from "../../src/modules/forgot_password/app/forgot_password_usecase";
 import { UserRepoMock } from "../../src/shared/infra/mocks/user_repository_mock";
 import { NoItemsFound } from "../../src/shared/helpers/errors/usecase_errors";
-import { UserMock } from "../../src/shared/domain/mocks/user_mock";
 import { EntityError } from "../../src/shared/helpers/errors/domain_errors";
+import * as mailSender from "../../src/shared/utils/mail_sender";
 
 describe("ForgotPasswordUseCase", () => {
   const userRepoMock = new UserRepoMock();
   const forgotPasswordUseCase = new ForgotPasswordUseCase(userRepoMock);
 
+  beforeEach(() => {
+    vi.clearAllMocks();
+    vi.restoreAllMocks();
+  });
+
   it("should throw NoItemsFound when user is not found", async () => {
     const email = "nonexistent@example.com";
-
     vi.spyOn(userRepoMock, "getUserByEmail").mockResolvedValue(null);
 
     await expect(forgotPasswordUseCase.execute(email)).rejects.toThrow(
@@ -29,17 +33,27 @@ describe("ForgotPasswordUseCase", () => {
   });
 
   it("should return a success message when user is found", async () => {
-    const email = "matue@30praum.com";
+    const email = userRepoMock.user_mock.users[0].userEmail;
     const user = userRepoMock.user_mock.users[0];
     vi.spyOn(userRepoMock, "getUserByEmail").mockResolvedValue(user);
     vi.spyOn(userRepoMock, "forgotPassword").mockResolvedValue(
       `A password reset link has been sent to ${email}.`
     );
+    const sendEmailMock = vi
+      .spyOn(mailSender, "sendEmail")
+      .mockResolvedValue(undefined);
 
     const result = await forgotPasswordUseCase.execute(email);
 
-    expect(result).toBe(`A password reset link has been sent to ${email}.`);
+    expect(result).toEqual({
+      message: "E-mail de recuperação enviado com sucesso",
+    });
     expect(userRepoMock.getUserByEmail).toHaveBeenCalledWith(email);
     expect(userRepoMock.forgotPassword).toHaveBeenCalledWith(email);
+    expect(sendEmailMock).toHaveBeenCalledWith(
+      email,
+      "Recuperação de Senha",
+      `Acesse esse link para trocar sua senha: A password reset link has been sent to ${email}.`
+    );
   });
 });
