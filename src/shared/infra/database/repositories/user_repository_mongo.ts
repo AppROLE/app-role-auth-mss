a729b90615fd5e5d30dc61db7e827184c8293b15
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-27 23:09:35 -0300

    refactor: Implement forgot password functionality in UserRepositoryMongo

--- src/shared/infra/database/repositories/user_repository_mongo.ts
+++ src/shared/infra/database/repositories/user_repository_mongo.ts
@@ -1,3 +1,22 @@
-export class UserRepositoryMongo {
-    
+import { User } from "../../../domain/entities/user";
+import { IUserRepository } from "../../../domain/interfaces/user_repository_interface";
+import userModel from "../models/user.model";
+
+export class UserRepositoryMongo implements IUserRepository {
+  getUserByEmail(email: string): Promise<User | null> {
+    return userModel.findOne({
+      userEmail: email,
+    });
+  }
+
+  // talvez seja cortado caso a função seja implementada 100% no mongol
+  public async forgotPassword(email: string): Promise<string> {
+    const user = await userModel.findOne({
+      userEmail: email,
+    });
+
+    // lógica de mandar o email seus pepeco, será nodemailer ou cognito?
+
+    return `Password reset email sent to ${email}`;
+  }
 }
