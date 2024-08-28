dcf99f6128be2c35f17778959c59c62536b2d7d3
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-28 10:45:28 -0300

    feat: Add email notification for password reset

--- src/modules/forgot_password/app/forgot_password_usecase.ts
+++ src/modules/forgot_password/app/forgot_password_usecase.ts
@@ -2,6 +2,7 @@ import { User } from "../../../shared/domain/entities/user";
 import { EntityError } from "../../../shared/helpers/errors/domain_errors";
 import { NoItemsFound } from "../../../shared/helpers/errors/usecase_errors";
 import { IUserRepository } from "../../../shared/domain/irepositories/user_repository_interface";
+import { sendEmail } from "../../../shared/utils/mail_sender";
 
 export class ForgotPasswordUseCase {
   constructor(private readonly repo: IUserRepository) {}
@@ -16,6 +17,20 @@ export class ForgotPasswordUseCase {
       throw new NoItemsFound("this user");
     }
 
-    return this.repo.forgotPassword(email);
+    const resetPasswordLink = await this.repo.forgotPassword(email);
+
+    try {
+      await sendEmail(
+        email,
+        "Recuperação de Senha",
+        `Acesse esse link para trocar sua senha: ${resetPasswordLink}`
+      );
+    } catch (error) {
+      throw new Error(
+        "Não foi possível enviar o e-mail de recuperação de senha."
+      );
+    }
+
+    return { message: "E-mail de recuperação enviado com sucesso" };
   }
 }
