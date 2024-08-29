980971588b4ef639c889a7c8179d9c5e4be69914
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-29 18:15:56 -0300

    refactor: Remove unused imports in forgot_password_presenter.ts and confirm_forgot_password_presenter.ts

--- src/modules/forgot_password/app/forgot_password_presenter.ts
+++ src/modules/forgot_password/app/forgot_password_presenter.ts
@@ -1,6 +1,3 @@
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Environments } from '../../../shared/environments'
 import {
   LambdaHttpRequest,
