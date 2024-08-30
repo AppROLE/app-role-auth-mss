a7c4f2eadb1f951156cb8d4274db26cbf845b819
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-30 12:22:58 -0300

    refactor: Add UserRepositoryMongo to correct repositories folder

---
+++ src/shared/infra/repositories/user/user_repository_mongo.ts
@@ -0,0 +1,22 @@
+import { User } from "../../../domain/entities/user";
+import { IUserRepository } from "../../../domain/irepositories/user_repository_interface";
+import userModel from "../../database/models/user.model";
+
+export class UserRepositoryMongo implements IUserRepository {
+  signUp(name: string, email: string, password: string, acceptedTerms: boolean): Promise<User> {
+    throw new Error("Method not implemented.");
+  }
+  getUserByEmail(email: string): Promise<User | null> {
+    return userModel.findOne({
+      userEmail: email,
+    });
+  }
+
+  public async forgotPassword(email: string): Promise<string> {
+    const user = await userModel.findOne({
+      userEmail: email,
+    });
+
+    return `Password reset email sent to ${email}`;
+  }
+}
