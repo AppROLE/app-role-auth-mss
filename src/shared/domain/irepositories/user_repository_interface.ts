8a1df55fcd148d0b782c6c2dcefc1bb3812f9285
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-08-31 12:28:05 -0300

    refactor: Update IUserRepository to include confirmCode method

--- src/shared/domain/irepositories/user_repository_interface.ts
+++ src/shared/domain/irepositories/user_repository_interface.ts
@@ -2,11 +2,15 @@ import { User } from "../entities/user";
 
 export interface IUserRepository {
   forgotPassword(email: string, code: string): Promise<string>;
-  getUserByEmail(email: string): Promise<User | null>;
+  getUserByEmail(email: string): Promise<User>;
   signUp(
     name: string,
     email: string,
     password: string,
     acceptedTerms: boolean
   ): Promise<User>;
+  confirmCode(email: string, code: string): Promise<{
+    user: User;
+    code: string;
+  }>;
 }
