5b8458eaab0fbed9bf5ee4fbf4d3a7e24dfb7f14
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-01 02:48:47 -0300

    refactor: Add ChangePasswordUseCase to handle change password functionality

---
+++ src/modules/change_password/app/change_password_usecase.ts
@@ -0,0 +1,21 @@
+import { IUserRepository } from "src/shared/domain/irepositories/user_repository_interface";
+import { EntityError } from "src/shared/helpers/errors/domain_errors";
+import { User } from "src/shared/domain/entities/user";
+
+export class ChangePasswordUseCase {
+  constructor(private readonly repo: IUserRepository) {}
+
+  async execute(email: string, newPassword: string) {
+    if (!User.validateEmail(email)) {
+      throw new EntityError("email");
+    }
+
+    if (!User.validatePassword(newPassword)) {
+      throw new EntityError("password");
+    }
+
+    await this.repo.setUserPassword(email, newPassword);
+
+    return { message: "Senha redefinida com sucesso!" };
+  }
+}
