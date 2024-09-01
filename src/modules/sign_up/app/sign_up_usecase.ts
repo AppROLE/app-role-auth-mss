266089e13a9b9ee5829e95216578e6200dfa3632
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-01 15:59:21 -0300

    refactor: Add error handling for duplicate email in SignUpUseCase and added CORRECT MAIL SENDING

--- src/modules/sign_up/app/sign_up_usecase.ts
+++ src/modules/sign_up/app/sign_up_usecase.ts
@@ -2,6 +2,7 @@ import { User } from "src/shared/domain/entities/user";
 import { IMailRepository } from "src/shared/domain/irepositories/mail_repository_interface";
 import { IUserRepository } from "src/shared/domain/irepositories/user_repository_interface";
 import { EntityError } from "src/shared/helpers/errors/domain_errors";
+import { DuplicatedItem } from "src/shared/helpers/errors/usecase_errors";
 
 export class SignUpUseCase {
   constructor(
@@ -30,7 +31,13 @@ export class SignUpUseCase {
       throw new EntityError("acceptedTerms");
     }
 
-    const createdUser = await this.repo.signUp(
+    const userAlreadyExists = await this.repo.getUserByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new DuplicatedItem("email")
+    }
+
+    const {user: createdUser, code} = await this.repo.signUp(
       name,
       email,
       password,
@@ -39,8 +46,8 @@ export class SignUpUseCase {
 
     await this.mailRepo.sendMail(
       email,
-      "Criação de conta",
-      `Olá ${name}, sua conta foi criada com sucesso!`
+      "Verificação de conta",
+      `Olá ${name}, sua conta foi criada com sucesso! Para confirmar seu cadastro, utilize o código: ${code}`
     );
 
     return createdUser;
