38f1a8ae7b6a92b55d62bf1f33b8d7447555e861
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-01 02:49:01 -0300

    refactor: Remove ConfirmForgotPasswordUsecase

--- src/modules/confirm_forgot_password/app/confirm_forgot_password_usecase.ts
+++
@@ -1,9 +0,0 @@
-import { IUserRepository } from "src/shared/domain/irepositories/user_repository_interface";
-
-export class ConfirmForgotPasswordUsecase {
-  constructor(private readonly repo: IUserRepository) {}
-
-  async execute(email: string, code: string, newPassword: string) {
-    return "Ok";
-  }
-}
