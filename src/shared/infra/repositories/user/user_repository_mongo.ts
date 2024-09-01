17b71ddc73f1994e0d572dc611b5d269a73edc43
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-01 03:02:51 -0300

    refactor: Update UserRepositoryMongo to include confirmCode and setUserPassword methods

--- src/shared/infra/repositories/user/user_repository_mongo.ts
+++ src/shared/infra/repositories/user/user_repository_mongo.ts
@@ -3,7 +3,24 @@ import { IUserRepository } from "../../../domain/irepositories/user_repository_i
 import userModel from "../../database/models/user.model";
 
 export class UserRepositoryMongo implements IUserRepository {
-  signUp(name: string, email: string, password: string, acceptedTerms: boolean): Promise<User> {
+  
+  confirmCode(
+    email: string,
+    code: string
+  ): Promise<{ user: User; code: string }> {
+    throw new Error("Method not implemented.");
+  }
+
+  setUserPassword(email: string, newPassword: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  signUp(
+    name: string,
+    email: string,
+    password: string,
+    acceptedTerms: boolean
+  ): Promise<User> {
     throw new Error("Method not implemented.");
   }
   getUserByEmail(email: string): Promise<User | null> {
