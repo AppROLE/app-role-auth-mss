5e00d35a41edcbb82a0b7d06e416d1a51f99d19c
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-01 02:59:32 -0300

    refactor: Update IUserRepository interface to return null for getUserByEmail when user is not found

--- src/shared/domain/irepositories/user_repository_interface.ts
+++ src/shared/domain/irepositories/user_repository_interface.ts
@@ -2,7 +2,7 @@ import { User } from "../entities/user";
 
 export interface IUserRepository {
   forgotPassword(email: string, code: string): Promise<string>;
-  getUserByEmail(email: string): Promise<User>;
+  getUserByEmail(email: string): Promise<User | null>;
   signUp(
     name: string,
     email: string,
@@ -11,7 +11,7 @@ export interface IUserRepository {
   ): Promise<User>;
   confirmCode(
     email: string,
-    code: string,
+    code: string
   ): Promise<{
     user: User;
     code: string;
