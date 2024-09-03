552eba326d177f5d4840517ae0eef5b090bae070
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-03 16:15:58 -0300

    refactor: Update auth repository import in confirm_forgot_password_usecase

--- src/modules/confirm_forgot_password/app/confirm_forgot_password_usecase.ts
+++ src/modules/confirm_forgot_password/app/confirm_forgot_password_usecase.ts
@@ -1,4 +1,4 @@
-import { IAuthRepository } from "src/shared/domain/irepositories/user_repository_interface";
+import { IAuthRepository } from "src/shared/domain/irepositories/auth_repository_interface";
 import { EntityError } from "src/shared/helpers/errors/domain_errors";
 import { User } from "src/shared/domain/entities/user";
 
