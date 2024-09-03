41aa6da371fd90c96c08d8a2c27f740e2ba19ab0
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-03 09:27:13 -0300

    refactor: Add FinishSignUpUseCase for handling finishing sign up process

---
+++ src/modules/finish_sign_up/app/finish_sign_up_usecase.ts
@@ -0,0 +1,37 @@
+import { User } from "src/shared/domain/entities/user";
+import { PRIVACY_TYPE } from "src/shared/domain/enums/privacy_enum";
+import { IAuthRepository } from "src/shared/domain/irepositories/auth_repository_interface";
+import { IUserRepository } from "src/shared/domain/irepositories/user_repository_interface";
+import { EntityError } from "src/shared/helpers/errors/domain_errors";
+
+export class FinishSignUpUseCase {
+  constructor(
+    private repo: IAuthRepository,
+    private mongoUserRepo: IUserRepository
+  ) {}
+
+  async execute(email: string, newUsername: string, password: string, newNickname?: string,) {
+    if (!User.validateEmail(email)) {
+      throw new EntityError("email");
+    }
+    if (!User.validateUsername(newUsername)) {
+      throw new EntityError("username");
+    }
+    if (newNickname && !User.validateNickname(newNickname)) {
+      throw new EntityError("nickname");
+    }
+
+    const userInfos = await this.repo.finishSignUp(email, newUsername, password, newNickname);
+
+    const userToMongo = new User({
+      email: userInfos.email,
+      name: userInfos.name,
+      username: userInfos.newUsername,
+      nickname: userInfos.newNickname,
+      privacy: PRIVACY_TYPE.PUBLIC,
+    })
+
+    await this.mongoUserRepo.createUser(userToMongo);
+
+  }
+}
