4a5e1e413f2b8e04d53db877230a559b8a333cee
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-03 18:16:38 -0300

    refactor: Update FinishSignUpViewmodel to include token properties

--- src/modules/finish_sign_up/app/finish_sign_up_viewmodel.ts
+++ src/modules/finish_sign_up/app/finish_sign_up_viewmodel.ts
@@ -1,13 +1,27 @@
 export class FinishSignUpViewmodel {
-  message: string;
+  access_token: string;
+  id_token: string;
+  refresh_token: string;
+  user_id?: string;
 
-  constructor(message: string) {
-    this.message = message;
+  constructor(
+    access_token: string,
+    id_token: string,
+    refresh_token: string,
+    user_id?: string
+  ) {
+    this.access_token = access_token;
+    this.id_token = id_token;
+    this.refresh_token = refresh_token;
+    this.user_id = user_id;
   }
 
-  toJSON() {
+  toJSON(): object {
     return {
-      message: this.message,
+      access_token: this.access_token,
+      id_token: this.id_token,
+      refresh_token: this.refresh_token,
+      user_id: this.user_id,
     };
   }
 }
