6f5d0600189fef8a97138490afb6b62ccfaa4c0f
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-03 09:28:01 -0300

    refactor: Update user repository interface to include createUser method

--- src/shared/domain/irepositories/user_repository_interface.ts
+++ src/shared/domain/irepositories/user_repository_interface.ts
@@ -1,24 +1,5 @@
 import { User } from "../entities/user";
 
 export interface IUserRepository {
-  forgotPassword(email: string, code: string): Promise<string>;
-  getUserByEmail(email: string): Promise<User | undefined>;
-  signUp(
-    name: string,
-    email: string,
-    password: string,
-    acceptedTerms: boolean
-  ): Promise<{
-    user: User;
-    code: string;
-  }>;
-  confirmCode(
-    email: string,
-    code: string
-  ): Promise<{
-    user: User;
-    code: string;
-  }>;
-  setUserPassword(email: string, newPassword: string): Promise<void>;
-  resendCode(email: string): Promise<string>;
+  createUser(user: User): Promise<User>;
 }
