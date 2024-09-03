5ec7df4917fad2607667baf6be15ca2bfca1a500
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-03 11:49:47 -0300

    refactor: Add SignInUseCase for handling sign-in requests

---
+++ src/modules/sign_in/app/sign_in_usecase.ts
@@ -0,0 +1,34 @@
+import { IAuthRepository } from "src/shared/domain/irepositories/auth_repository_interface";
+import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
+import { User } from "src/shared/domain/entities/user";
+import { EntityError } from "src/shared/helpers/errors/domain_errors";
+import { InvalidCredentialsError } from "src/shared/helpers/errors/login_errors";
+
+export class SignInUseCase {
+  constructor(private readonly repo: IAuthRepository) {}
+
+  async execute(email: string, password: string) {
+    if (!email) {
+      throw new NoItemsFound("email");
+    }
+
+    if (!password) {
+      throw new NoItemsFound("password");
+    }
+
+    if (!User.validateEmail(email)) {
+      throw new EntityError("email");
+    }
+
+    if (!User.validatePassword(password)) {
+      throw new EntityError("password");
+    }
+
+    const session = await this.repo.signIn(email, password);
+
+    if (!session) {
+      throw new InvalidCredentialsError();
+    }
+    return session;
+  }
+}
