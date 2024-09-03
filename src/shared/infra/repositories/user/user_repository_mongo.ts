c5ea75a5763f96c852a875857a9cd167cbec45f7
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-03 09:28:34 -0300

    refactor: Add createUser method to UserRepositoryMongo

--- src/shared/infra/repositories/user/user_repository_mongo.ts
+++ src/shared/infra/repositories/user/user_repository_mongo.ts
@@ -1,43 +1,19 @@
 import { User } from "../../../domain/entities/user";
 import { IUserRepository } from "../../../domain/irepositories/user_repository_interface";
 import userModel from "../../database/models/user.model";
+import { UserMongoDTO } from "../../dto/user_mongo_dto"
 
 export class UserRepositoryMongo implements IUserRepository {
-  resendCode(email: string): Promise<string> {
-    throw new Error("Method not implemented.");
-  }
+  async createUser(user: User): Promise<User> {
+    try {
+      const dto = UserMongoDTO.fromEntity(user);
+      const userDoc = UserMongoDTO.toMongo(dto);
 
-  confirmCode(
-    email: string,
-    code: string
-  ): Promise<{ user: User; code: string }> {
-    throw new Error("Method not implemented.");
-  }
+      await userModel.create(userDoc);
 
-  setUserPassword(email: string, newPassword: string): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-
-  signUp(
-    name: string,
-    email: string,
-    password: string,
-    acceptedTerms: boolean
-  ): Promise<{
-    user: User;
-    code: string;
-  }> {
-    throw new Error("Method not implemented.");
-  }
-  getUserByEmail(email: string): Promise<User | undefined> {
-    throw new Error("Method not implemented.");
-  }
-
-  public async forgotPassword(email: string): Promise<string> {
-    const user = await userModel.findOne({
-      userEmail: email,
-    });
-
-    return `Password reset email sent to ${email}`;
+      return user;
+    } catch (error) {
+      throw new Error(`Error creating user on MongoDB: ${error}`);
+    }
   }
 }
