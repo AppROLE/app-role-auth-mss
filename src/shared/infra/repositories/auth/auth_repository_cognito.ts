d8ba09d588d554238041e90a4f1231961ff59046
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-04 02:15:29 -0300

    refactor: Improve error handling and parameter validation in AuthRepositoryCognito

--- src/shared/infra/repositories/auth/auth_repository_cognito.ts
+++ src/shared/infra/repositories/auth/auth_repository_cognito.ts
@@ -235,7 +235,7 @@ export class AuthRepositoryCognito implements IAuthRepository {
         throw new EntityError("code");
       }
 
-      if (!isUserEmailVerified) {
+      if (isUserEmailVerified === "false") {
         const paramsConfirmEmail: AdminUpdateUserAttributesCommandInput = {
           UserPoolId: this.userPoolId,
           Username: user.userUsername as string,
@@ -251,16 +251,17 @@ export class AuthRepositoryCognito implements IAuthRepository {
           paramsConfirmEmail
         );
         await this.client.send(commandConfirmEmail);
+        
+        const paramsAdminConfirmSignUp: AdminConfirmSignUpCommandInput = {
+          UserPoolId: this.userPoolId,
+          Username: user.userUsername as string,
+        };
+  
+        await this.client.send(
+          new AdminConfirmSignUpCommand(paramsAdminConfirmSignUp)
+        );
       }
 
-      const paramsAdminConfirmSignUp: AdminConfirmSignUpCommandInput = {
-        UserPoolId: this.userPoolId,
-        Username: user.userUsername as string,
-      };
-
-      await this.client.send(
-        new AdminConfirmSignUpCommand(paramsAdminConfirmSignUp)
-      );
       return { user, code };
     } catch (error: any) {
       throw new Error(
