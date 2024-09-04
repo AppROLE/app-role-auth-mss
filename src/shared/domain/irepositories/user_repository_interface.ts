dce3581ab83115615510a600997a72f5eaca4457
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-04 17:42:49 -0300

    feat: Add updateProfilePhoto method to IUserRepository interface

--- src/shared/domain/irepositories/user_repository_interface.ts
+++ src/shared/domain/irepositories/user_repository_interface.ts
@@ -2,4 +2,5 @@ import { User } from "../entities/user";
 
 export interface IUserRepository {
   createUser(user: User): Promise<User>;
+  updateProfilePhoto(email: string, profilePhoto: string): Promise<string>;
 }
