7453554af77acd65eaa09a416f9798745c3ac3bb
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-04 13:28:08 -0300

    refactor: Update FinishSignUpViewmodel to use a message property instead of individual token properties

--- src/modules/finish_sign_up/app/finish_sign_up_viewmodel.ts
+++ src/modules/finish_sign_up/app/finish_sign_up_viewmodel.ts
@@ -1,27 +1,15 @@
 export class FinishSignUpViewmodel {
-  access_token: string;
-  id_token: string;
-  refresh_token: string;
-  user_id?: string;
+  message: string;
 
   constructor(
-    access_token: string,
-    id_token: string,
-    refresh_token: string,
-    user_id?: string
+    message: string,
   ) {
-    this.access_token = access_token;
-    this.id_token = id_token;
-    this.refresh_token = refresh_token;
-    this.user_id = user_id;
+    this.message = message;
   }
 
   toJSON(): object {
     return {
-      access_token: this.access_token,
-      id_token: this.id_token,
-      refresh_token: this.refresh_token,
-      user_id: this.user_id,
+      message: this.message,
     };
   }
 }
