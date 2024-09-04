bd94a50127fc58593261b257c3d1762427303591
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-04 17:40:10 -0300

    refactor: Update npm dependencies, add busboy package, and improve error handling and logging in FinishSignUpController

--- iac/iac/iac_stack.ts
+++ iac/iac/iac_stack.ts
@@ -32,36 +32,6 @@ export class IacStack extends Stack {
         allowHeaders: Cors.DEFAULT_HEADERS,
       },
     });
-
-    const cognitoStack = new CognitoStack(this, `${envs.STACK_NAME}-CognitoStack`);
-
-    const environmentVariables = {
-      STAGE: stage,
-      NODE_PATH: '/var/task:/opt/nodejs',
-      COGNITO_USER_POOL_ID: cognitoStack.userPool.userPoolId,
-      COGNITO_CLIENT_ID: cognitoStack.client.userPoolClientId,
-      EMAIL_LOGIN: envs.EMAIL_LOGIN,
-      EMAIL_PASSWORD: envs.EMAIL_PASSWORD,
-      MONGO_URI: envs.MONGO_URI,
-      S3_BUCKET_NAME: envs.S3_BUCKET_NAME + "-" + stage.toLowerCase(),
-    };
-
-    const lambdaStack = new LambdaStack(
-      this,
-      apigatewayResource,
-      environmentVariables
-    )
-
-    const cognitoAdminPolicy = new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: ["cognito-idp:*"],
-      resources: [cognitoStack.userPool.userPoolArn],
-    });
-
-    for (const fn of lambdaStack.functionsThatNeedCognitoPermissions) {
-      fn.addToRolePolicy(cognitoAdminPolicy);
-    }
-
     const s3Stack = new S3Stack(this, `${envs.STACK_NAME}-S3Stack`);
 
     const s3Policy = new iam.PolicyStatement({
@@ -70,14 +40,11 @@ export class IacStack extends Stack {
       resources: [s3Stack.bucket.bucketArn],
     });
 
-    for (const fn of lambdaStack.functionsThatNeedS3Permissions) {
-      fn.addToRolePolicy(s3Policy);
-    }
-
+    
     const originAccessIdentity = new OriginAccessIdentity(this, 'AppRoleFrontOAI', {
       comment: 'OAI for S3 bucket',
     });
-
+    
     const distribution = new Distribution(this, 'AppRoleFrontDistribution', {
       defaultBehavior: {
         origin: new cdk.aws_cloudfront_origins.S3Origin(s3Stack.bucket, {
@@ -89,9 +56,9 @@ export class IacStack extends Stack {
         cachePolicy: cdk.aws_cloudfront.CachePolicy.CACHING_OPTIMIZED,
       },
     });
-
+    
     distribution.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
-
+    
     s3Stack.bucket.addToResourcePolicy(new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
       actions: ['s3:GetObject'],
@@ -104,6 +71,40 @@ export class IacStack extends Stack {
         },
       })],
     }));
+    
+    const cognitoStack = new CognitoStack(this, `${envs.STACK_NAME}-CognitoStack`);
+    
+    const environmentVariables = {
+      STAGE: stage,
+      NODE_PATH: '/var/task:/opt/nodejs',
+      COGNITO_USER_POOL_ID: cognitoStack.userPool.userPoolId,
+      COGNITO_CLIENT_ID: cognitoStack.client.userPoolClientId,
+      EMAIL_LOGIN: envs.EMAIL_LOGIN,
+      EMAIL_PASSWORD: envs.EMAIL_PASSWORD,
+      MONGO_URI: envs.MONGO_URI,
+      S3_BUCKET_NAME: envs.S3_BUCKET_NAME + "-" + stage.toLowerCase(),
+      CLOUDFRONT_DISTRO: distribution.domainName,
+    };
+    
+    const lambdaStack = new LambdaStack(
+      this,
+      apigatewayResource,
+      environmentVariables
+    )
+    
+    const cognitoAdminPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["cognito-idp:*"],
+      resources: [cognitoStack.userPool.userPoolArn],
+    });
+    
+    for (const fn of lambdaStack.functionsThatNeedCognitoPermissions) {
+      fn.addToRolePolicy(cognitoAdminPolicy);
+    }
+    
+    for (const fn of lambdaStack.functionsThatNeedS3Permissions) {
+      fn.addToRolePolicy(s3Policy);
+    }
 
     new cdk.CfnOutput(this, 'AppRoleFrontDistributionOutput', {
       value: distribution.domainName,
