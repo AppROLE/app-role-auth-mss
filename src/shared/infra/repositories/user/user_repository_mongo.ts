8005f3862709fe113196f5b09109d532dce862dd
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-04 12:00:18 -0300

    refactor: Update UserRepositoryMongo createUser method to include MongoDB connection and logging

--- src/shared/infra/repositories/user/user_repository_mongo.ts
+++ src/shared/infra/repositories/user/user_repository_mongo.ts
@@ -7,15 +7,16 @@ import { UserMongoDTO } from "../../dto/user_mongo_dto"
 export class UserRepositoryMongo implements IUserRepository { 
   async createUser(user: User): Promise<User> {
     try {
-      await connectDB();
+      const db = await connectDB();
+      console.log('MONGO REPO DB: ', db);
 
       const dto = UserMongoDTO.fromEntity(user);
       const userDoc = UserMongoDTO.toMongo(dto);
 
       console.log('MONGO REPO USER DOC: ', userDoc);
 
-      await userModel.create(userDoc);
-
+      const respMongo = await userModel.create(userDoc);
+      console.log('MONGO REPO USER RESPMONGO: ', respMongo);
       console.log('MONGO REPO USER CREATED: ', user);
 
       return user;
