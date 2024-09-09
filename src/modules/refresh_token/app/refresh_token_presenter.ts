146da607fdbb213e3ddfe656227d1969bba78445
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-08 23:18:15 -0300

    refactor: Add confirmCodePresenter function to handle code confirmation requests

---
+++ src/modules/refresh_token/app/refresh_token_presenter.ts
@@ -0,0 +1,31 @@
+import { Environments } from "src/shared/environments";
+import {
+  LambdaHttpRequest,
+  LambdaHttpResponse,
+} from "src/shared/helpers/external_interfaces/http_lambda_requests";
+import { RefreshTokenUseCase } from "./refresh_token_usecase";
+import { RefreshTokenController } from "./refresh_token_controller";
+
+const repo = Environments.getAuthRepo();
+const usecase = new RefreshTokenUseCase(repo);
+const controller = new RefreshTokenController(usecase);
+
+export async function confirmCodePresenter(
+  event: Record<string, any>
+) {
+  const httpRequest = new LambdaHttpRequest(event);
+  const response = await controller.handle(httpRequest);
+  const httpResponse = new LambdaHttpResponse(
+    response?.body,
+    response?.statusCode,
+    response?.headers
+  );
+  
+
+  return httpResponse.toJSON();
+}
+
+export async function lambda_handler(event: any, context: any) {
+  const response = await confirmCodePresenter(event);
+  return response;
+}
