a76a5c845f3225c1c95fc566b34860590bd1610a
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-09 01:19:32 -0300

    refactor: Update GetProfileController to include GetProfileViewmodel in response

--- src/modules/get_profile/app/get_profile_controller.ts
+++ src/modules/get_profile/app/get_profile_controller.ts
@@ -1,7 +1,8 @@
 import { IRequest } from "src/shared/helpers/external_interfaces/external_interface";
 import { GetProfileUseCase } from "./get_profile_usecase";
-import { InternalServerError } from "src/shared/helpers/external_interfaces/http_codes";
+import { InternalServerError, OK } from "src/shared/helpers/external_interfaces/http_codes";
 import { UserAPIGatewayDTO } from "src/shared/infra/dto/user_api_gateway_dto";
+import { GetProfileViewmodel } from "./get_profile_viewmodel";
 
 export class GetProfileController {
   constructor(private readonly usecase: GetProfileUseCase) {}
@@ -16,7 +17,15 @@ export class GetProfileController {
       console.log('username after api gateway dto', username);
       const profile = await this.usecase.execute(username);
 
+      const viewmodel = new GetProfileViewmodel(
+        profile.userId,
+        profile.name,
+        profile.username,
+        profile.following,
+        profile.followers,
+      )
 
+      return new OK(viewmodel.toJSON())
 
     } catch (error: any) {
       if (error instanceof Error) {
