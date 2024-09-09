53baf8df4c0d37fba1a123c377dafc36991bce48
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-09 00:34:13 -0300

    refactor: Add getProfile method to IUserRepository interface

--- src/shared/domain/irepositories/user_repository_interface.ts
+++ src/shared/domain/irepositories/user_repository_interface.ts
@@ -1,6 +1,8 @@
+import { GetProfileReturnType } from "src/shared/helpers/types/get_profile_return_type";
 import { User } from "../entities/user";
 
 export interface IUserRepository {
   createUser(user: User): Promise<User>;
   updateProfilePhoto(email: string, profilePhoto: string): Promise<string>;
+  getProfile(username: string): Promise<GetProfileReturnType>
 }
