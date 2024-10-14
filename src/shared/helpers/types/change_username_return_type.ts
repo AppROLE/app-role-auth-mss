1c30acfb28c49828905fff9bd0923017e28f87cc
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-14 07:14:49 -0300

    feat: Add ChangeUsernameReturnType type for user profile updates

---
+++ src/shared/helpers/types/change_username_return_type.ts
@@ -0,0 +1,7 @@
+export type ChangeUsernameReturnType = {
+  email: string | undefined,
+  name: string | undefined,
+  nickname: string | undefined,
+  acceptedTerms: string | undefined,
+  roleType: string | undefined,
+}
