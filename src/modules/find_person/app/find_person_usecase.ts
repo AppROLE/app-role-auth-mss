5ebf66117ee2047dadb3ff809a420337a9703663
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-14 01:32:10 -0300

    Improve person search functionality in UserRepositoryMongo

--- src/modules/find_person/app/find_person_usecase.ts
+++ src/modules/find_person/app/find_person_usecase.ts
@@ -1,10 +1,15 @@
 import { IUserRepository } from "src/shared/domain/irepositories/user_repository_interface";
+import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
 
 export class FindPersonUseCase { 
   constructor(private readonly userRepo: IUserRepository) {}
 
   async execute(searchTerm: string) {
     const persons = await this.userRepo.findPerson(searchTerm);
+
+    if (persons.length === 0) {
+      throw new NoItemsFound('pessoas');
+    }
 
     return persons;
   }
